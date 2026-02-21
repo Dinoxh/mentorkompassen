92f78fe2ec562c21c2361775eae0138bf33ee62e
Sami Al Saati <sami.alsaati@hotmail.com>
2026-02-21 14:38:44 +0100

    Added styling for component

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,5 +1,13 @@
 import { QuizBox } from '@/features/quiz/components/quiz-box.tsx'
 
 export function App() {
-  return <QuizBox header="Quiz App" question={''} options={[]}></QuizBox>
+  return (
+    <main className="min-h-screen bg-[#EFEEE7] flex items-center justify-center">
+      <QuizBox
+        header="Mentor kompassen"
+        question="What are you doing today?"
+        options={[]}
+      ></QuizBox>
+    </main>
+  )
 }
--- src/features/quiz/components/quiz-box.tsx
+++ src/features/quiz/components/quiz-box.tsx
@@ -4,10 +4,12 @@ type QuizBoxProps = {
   options: string[]
 }
 
-export function QuizBox({ header }: React.PropsWithChildren<QuizBoxProps>) {
+export function QuizBox({ header, question }: React.PropsWithChildren<QuizBoxProps>) {
   return (
-    <div className="bg-white rounded-lg shadow-md p-6">
-      <h1 className="text-2xl font-bold mb-4">{header}</h1>
+    <div className="relative max-w-[240px] max-h-[880px] rounded-[56px] bg-white px-60 py-40">
+      <h1 className=" text-4xl absolute top-4 left-1/2 -translate-x-1/2  font-bold">{header}</h1>
+
+      <h2 className="text-lg font-semibold ">{question}</h2>
     </div>
   )
 }
